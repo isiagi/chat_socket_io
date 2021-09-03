64d1cfa8f44043d21fd42adef153518beceae958
isiagi <isiagigeofrey0@gmail.com>
2021-09-03 15:05:03 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require('path')
+const app = express();
+
+const PORT = process.env.PORT || 5000
+
+const server = app.listen(PORT, ()=>{
+    console.log(`we are listening at PORT ${PORT}`);
+})
+
+let socketsConnected = new Set()
+
+const io = require('socket.io')(server)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+io.on('connection', onConnected)
+
+function onConnected(socket){
+    console.log(socket.id);
+    socketsConnected.add(socket.id)
+
+    io.emit('client-total', socketsConnected.size)
+
+    socket.on('message', (data) => {
+        console.log(data);
+        socket.broadcast.emit('chat-message', data)
+    })
+
+    socket.on('feedback', (data) => {
+        socket.broadcast.emit('feedback', data)
+    })
+
+    socket.on('disconnect', ()=> {
+        socketsConnected.delete(socket.id);
+        io.emit('client-total', socketsConnected.size)
+    })
+}
